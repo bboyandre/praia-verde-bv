07068f1fcb53bea9c73c1ed9fde302b62c851ccf
andrem3 <https://andrem3@bitbucket.org/>
2022-06-16 18:14:22 -0300

    Componente Start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Start from './Start/index'
+
+function App() {
+
+  return (
+    <>
+      <Start />
+    </>
+  )
+}
+
+export default App
---
+++ src/Start/index.jsx
@@ -0,0 +1,15 @@
+import './style.scss'
+import logo from '../assets/brasao-prefeitura-municipal-de-boa-vista-rr 1.png'
+
+const Start = ()=> {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo}></img>
+                <p>PRAIA VERDE BV</p>
+            </header>
+        </div>
+  )
+}
+
+export default Start
